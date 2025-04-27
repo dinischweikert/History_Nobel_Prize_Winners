d1dacdae1b92c9758f20a942388c53147ab7c5fc
dinischweikert <80862736+dinischweikert@users.noreply.github.com>
2025-04-27 16:26:30 -0500

    Update Nobel_Prize_Prog.py
    Add comments and better breaks.

--- Nobel_Prize_Prog.py
+++ Nobel_Prize_Prog.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# The Nobel Prize has been among the most prestigious international awards since 1901. Each year, awards are bestowed in chemistry, literature, physics, physiology or medicine, economics, and peace. In addition to the honor, prestige, and substantial prize money, the recipient also gets a gold medal with an image of Alfred Nobel (1833 - 1896), who established the prize.
+# The Nobel Prize has been among the most prestigious international awards since 1901. Each year, 
+# awards are bestowed in chemistry, literature, physics, physiology or medicine, economics, and peace. 
+# In addition to the honor, prestige, and substantial prize money, the recipient also gets a gold medal 
+# with an image of Alfred Nobel (1833 - 1896), who established the prize.
 # 
-# ![](Nobel_Prize.png)
-# 
-# The Nobel Foundation has made a dataset available of all prize winners from the outset of the awards from 1901 to 2023. The dataset used in this project is from the Nobel Prize API and is available in the `nobel.csv` file in the `data` folder.
-# 
-# In this project, we explore and answer several questions related to this prizewinning data. 
+# The Nobel Foundation has made a dataset available of all prize winners from the outset of the awards 
+# from 1901 to 2023. The dataset used in this project is from the Nobel Prize API and is available 
+# in the `nobel.csv` file. In this project, we explore and answer several questions related to this 
+# prizewinning data. 
 
 
 # Loading in required libraries
@@ -18,12 +20,15 @@ import numpy as np
 # Read in data
 df = pd.read_csv('nobel.csv')
 
+#######################################################################################################
 # most commonly awarded gender and birth country
+#######################################################################################################
 top_gender = df['sex'].mode()
 top_country = df['birth_country'].mode()
 
-# decade with the highest ratio of US-born nobel prize winners to total winners
-# split out decades
+#######################################################################################################
+# decade with the highest ratio of US-born nobel prize winners to total winners split out decades
+#######################################################################################################
 def get_decade(year):
     return year // 10 * 10
 
@@ -34,9 +39,9 @@ prop_usa_winners = df.groupby('decade', as_index=False)['usa_born_winner'].mean(
 # highest ratio of US-born nobel prize winners
 max_decade_usa = prop_usa_winners[prop_usa_winners['usa_born_winner'] == prop_usa_winners['usa_born_winner'].max()]['decade'].values[0]
 
-
-# decade and nobel prize category combination had highest proportion 
-# of female winners
+#######################################################################################################
+# decade and nobel prize category combination had highest proportion of female winners
+#######################################################################################################
 df['female_winner'] = df['sex'] == 'Female'
 
 prop_female_winner = df.groupby(['decade', 'category'],as_index=False)['female_winner'].mean()
@@ -47,11 +52,25 @@ category_value = prop_female_winner.loc[prop_female_winner['female_winner'] == p
 
 max_female_dict = {decade_key: category_value}
 
+#######################################################################################################
+# first woman to receive a nobel price and in what category
+#######################################################################################################
+female_df = df.loc[df['sex'] == 'Female']
+
+# first woman name
+first_woman_name = female_df.loc[female_df['year'] == female_df['year'].min()]['full_name'].values[0]
+
+# first woman category
+first_woman_category = female_df.loc[female_df['year'] == female_df['year'].min()]['category'].values[0]
+
+#######################################################################################################
 # individuals/organizations that have won more than 1 nobel prize
+#######################################################################################################
 name_list = []
 repeat_list = []
 name_list = df['full_name'].unique()
 for name in name_list:
     if len(df.loc[df['full_name']==name]) > 1:
         repeat_list.append(name)
+
 
